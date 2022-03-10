0747084ae1aa98c86e86604701d8e2d7edfcce7a
moshaban <moshaban@SH-MOSTAFA-SHAB>
2022-03-10 14:13:35 +0200

    add video

--- src/Projects.js
+++ src/Projects.js
@@ -13,53 +13,62 @@ import kms from "./assets/video/kms.mp4";
 
 const webApps = [
   {
-    name: "kms",
+    name: "KMS",
     url: "",
-    // imgSrc: kms,
+    imgSrc: null,
     videoSrc: kms,
     description:
-      "knowledge management system, Helps facilitate searches for researchers in all sectors",
-  },
-  {
-    name: "popc",
-    url: "http://popc.idsc.gov.eg/",
-    imgSrc: popc,
-    description:
-      "Studying the attitudes of Egyptian public opinion towards the issues raised on the internal scene, and directing them to the decision maker",
+      "Knowledge Management System is a web application helps facilitate searches for researchers in all sectors.",
+    tech: 'the application built with React JS Library for frontend, and ASP.NET Core for backend'
   },
   {
     name: "agriprice",
     url: "http://agriprice.gov.eg",
     imgSrc: agriprice,
     description:
-      "Show monitored local and international commodities prices application built with React js and ASP.Net Core.",
+      "Agriprice is a Web Site that Shows monitored local and international commodities prices .",
+      tech: 'the application built with React JS Library for frontend, and ASP.NET Core for backend'
+  },
+  {
+    name: "Poll Center",
+    url: "http://popc.idsc.gov.eg/",
+    imgSrc: popc,
+    description:
+      "Poll Center is a Website that Studys the attitudes of Egyptian public opinion towards the issues raised on the internal scene, and directing them to the decision maker.",
+    tech: 'the application built with ASP.NET MVC, using JavaScript, Jquery and ajax for frontend.'
   },
   {
     name: "idsc",
     url: "https://idsc.gov.eg/IDSC/Default.aspx",
     imgSrc: idsc,
     description:
-      "The official platform for the online portal of IDSC built with web forms.",
+      "IDSC Website is The official platform for the online portal of Information and Decision Support Center.",
+    tech: 'the application built with ASP.NET MVC, using JavaScript, Jquery and ajax for frontend.'
   },
   {
     name: "aswan",
     url: "https://aswan.gov.eg/",
     imgSrc: aswan,
     description:
-      "The official platform for the online portal of Aswan governerate built with React js and ASP.Net Core.",
+      "Aswan Website is The official platform for the online Portal of Aswan governerate.",
+    tech: 'the application built with React JS Library for frontend, and ASP.NET Core for backend'
+
   },
   {
-    name: "library",
+    name: "idsc library",
     url: "http://www.library.idsc.gov.eg",
     imgSrc: library,
     description:
-      "Accessing the Library remotely to make its collections, experts, and services more readily discoverable and available for users, application built with ASP.Net Core.",
-  },
+      "Accessing the Library remotely to make its collections, experts, and services more readily discoverable and available for users.",
+    tech: 'the application built with ASP.NET MVC, using JavaScript, Jquery and ajax for frontend.'
+    },
   {
     name: "tbv",
     url: "https://jobs.tbv.cloud/aa/Teqneia",
     imgSrc: tbv,
-    description: "Online recruitment application built with React and php.",
+    description: "Online recruitment application to make it easier to source candidates and conduct interviews as well as process the relevant paperwork required to hire and train candidates",
+    tech: 'the application built with React JS Library for frontend, and PHP Core for backend'
+  
   },
 ];
 const Projects = () => {
@@ -67,10 +76,10 @@ const Projects = () => {
     <Container>
       {webApps.map((item, index) => {
         return (
-          <Row className="my-4">
+          <Row className="my-4" key={index}>
             <Col lg={8}>
-              <a href={item.url} target="_blank" rel="noreferrer">
-                <h3>{item.name}</h3>
+              <a href={item.url ? item.url : '#' } target="_blank" rel="noreferrer">
+                <h3 style={{textTransform: 'uppercase'}}>{item.name}</h3>
               </a>
               {item.imgSrc ?  <img
                 style={{
@@ -80,9 +89,9 @@ const Projects = () => {
                 className="img-fluid my-3"
                 src={item.imgSrc}
                 alt={item.imgSrc}
-              /> : <video width="750" height="500" controls ><source src={item.videoSrc} type="video/mp4"/></video> }
-             <source src={item.videoSrc} type="video/mp4"/> 
+              /> : <video preload='auto'   style={{boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", borderRadius: "8px",}} width="100%" controls ><source src={item.videoSrc} type="video/mp4"/></video> }   
               <p className="my-4">{item.description}</p>
+              <p className="my-4">{item.tech}</p>
             </Col>
           </Row>
         );
