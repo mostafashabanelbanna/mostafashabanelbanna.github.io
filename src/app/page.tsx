f4d741465967a699e968c29dc4b8dab25936ee60
mostafashabanelbanna <mostafashabanelbanna@gmail.com>
2025-06-18 09:48:03 +0300

    fix building issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,7 @@ import Projects from "../components/Projects";
 import Experience from "../components/Experience";
 import Sidebar from "@/components/Sidebar";
 
-export default function HomeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function HomeLayout() {
   return (
     <div className="min-h-screen bg-[color:var(--background)] text-[color:var(--primary-text)] font-sans">
       <div className="flex container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
